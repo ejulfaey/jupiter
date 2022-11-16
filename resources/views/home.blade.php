f2931c496cd3c78aba4a6b975451a4be1cd01f19
ejulfaey <ejulfae@gmail.com>
2022-11-16 23:01:53 +0800

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Jupiter</title>
+    <link rel="shortcut icon" href="/jupiter-32.png" type="image/x-icon">
+    <script src="https://cdn.tailwindcss.com"></script>
+</head>
+
+<body class="antialiased bg-gray-100">
+    
+</body>
+
+</html>
